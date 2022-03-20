3a14c37ea5148604b5913f9ae4e8966e594af8d5
amada10 <amada.rktvo@gmail.com>
2022-03-21 02:22:27 +0300

    [ADD] mise a jour favicon

--- front/src/components/Cards/CardAddContenu.js
+++ front/src/components/Cards/CardAddContenu.js
@@ -115,7 +115,7 @@ export default function CardAddContenu() {
                          <option  hidden>Choisir le type de contenu</option>
                          <option key="1" value="galerie"> galerie</option>
                          <option key="2" value="emploi">offre d'emploi</option>
-                         <option key="3" value="information">information</option>
+                         <option key="3" value="actualite">actualité</option>
                     </select>
                     <p className="text-red-500 italic">{errors.type?.message}</p>
                   </div>

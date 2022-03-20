c43f2abb225522bdaf455f3df1f5d4cac34ddecb
root <root@_HOSTNAME_>
2022-03-21 00:21:33 +0300

    [ADD] change back to prod link

--- front/src/utils/urlAxios/UrlAxios.js
+++ front/src/utils/urlAxios/UrlAxios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const RouteAxios = Axios.create({
-    baseURL: "http://localhost:5000/api/v1" /*route v1 with endpoin '/login' && 'add_account' */
+    baseURL: "https://forum-api.iteam-s.mg/api/v1" /*route v1 with endpoin '/login' && 'add_account' */
 })
 
 export {

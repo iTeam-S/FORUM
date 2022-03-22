c2d3a2c308a05a75545a703e9224b45638423903
amada10 <amada.rktvo@gmail.com>
2022-03-22 23:37:02 +0300

    [FIX] url

--- front/src/utils/urlAxios/UrlAxios.js
+++ front/src/utils/urlAxios/UrlAxios.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
-const uRI = "http://localhost:5000/api/v1"
+//const uRI = "http://localhost:5000/api/v1"
+const uRI = "https://forum-api.iteam-s.mg/api/v1"
 
 const RouteAxios = Axios.create({
     baseURL: uRI /*route v1 with endpoin '/login' && 'add_account' */

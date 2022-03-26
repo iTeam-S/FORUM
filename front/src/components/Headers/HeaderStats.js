e341e1dbb9e3d5c47647c964abc5714d851d7e88
amada10 <amada.rktvo@gmail.com>
2022-03-26 13:35:53 +0300

    [FIX] reglage header

--- front/src/components/Headers/HeaderStats.js
+++ front/src/components/Headers/HeaderStats.js
@@ -4,23 +4,35 @@ import React, {useContext} from "react";
 
 import CardStats from "components/Cards/CardStats.js";
 import { CompteContext } from "utils/contexte/CompteContext";
+import { LoginService } from "utils/service/LoginService";
 
 
 export default function HeaderStats() {
   let totalCompte, totalContenu, totalVisiteur = 0;
+  let id = LoginService.getCurrentCompte().id;
   const {contenus} = useContext(CompteContext);
   const {compte} = useContext(CompteContext);
   const type = JSON.parse(localStorage.getItem('compte')).type;
+
+  let visiteurEntreprise = compte
+                          .filter((item) => {
+                            return item.id === id;
+                          })
+                          .map((item) => {
+                            return item.visiteurs;
+                          })
+  
 
   totalCompte = compte.length;
   totalContenu = contenus.length;
 
   //total visiteur
   let visiteur = compte.map((account) => {
-    return account.visiteurs
+    return account.visiteurs;
   })
 
   totalVisiteur = visiteur.reduce((a, b) => a + b, 0);
+  
 
 
   return (
@@ -40,14 +52,27 @@ export default function HeaderStats() {
                       statIconColor="bg-red-500"
                     />
                   </div>}
-              <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
-                <CardStats
-                  statSubtitle="Visiteurs stands"
-                  statTitle={totalVisiteur.toString()}
-                  statIconName="fas fa-users"
-                  statIconColor="bg-pink-500"
-                />
-              </div>
+              { type === "ADMIN" ? (
+                  <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
+                    <CardStats
+                      statSubtitle="Tous les visiteurs"
+                      statTitle={totalVisiteur.toString()}
+                      statIconName="fas fa-users"
+                      statIconColor="bg-pink-500"
+                    />
+                  </div>
+                ) : (
+                    <div className="w-full lg:w-6/12 xl:w-4/12 px-4">
+                      <CardStats
+                        statSubtitle="Visiteurs stands"
+                        statTitle={visiteurEntreprise.toString()}
+                        statIconName="fas fa-users"
+                        statIconColor="bg-pink-500"
+                      />
+                    </div>
+                )
+
+              }
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="Contenus"
